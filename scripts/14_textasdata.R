722c44ce3e2bf9def632e918b6f1d835b08b8d45
Frederik Hjorth <frederikhjorth@gmail.com>
2016-12-08 09:21:18 +0100

    fixet eksempel

--- scripts/14_textasdata.R
+++ scripts/14_textasdata.R
@@ -4,16 +4,25 @@ require(quanteda)
 
 #motiverende eksempel: meget simple tekster
 
-simpletexts<-list(el=c("velfærd velfærd velfærd"),
-                  sd=c("velfærd velfærd vækst"),
-                  v=c("velfærd vækst vækst"),
-                  la=c("vækst vækst vækst"))
+simpletexts<-c("velfærd velfærd velfærd",
+               "velfærd velfærd vækst",
+               "velfærd vækst vækst",
+               "vækst vækst vækst")
+simplecorpus<-corpus(simpletexts,docnames=c("el","s","v","la"))
+simpledfm<-dfm(simpletexts)
 
-simplecorpus<-corpus(unlist(simpletexts),docnames=names(simpletexts))
-
-simpledfm<-dfm(unlist(simpletexts))
+#tf-idf
 
 tfidf(simpledfm)
 
-simplewf<-textmodel_wordfish(simpledfm,dir=c(1,4))
-simplews<-textmodel_wordscores(simpledfm,)
+#regner efter i hånden
+tf<-3
+idf<- log10( 4/3 )
+tf*idf
+
+#wordscores eksempel
+
+simplews<-textmodel(x=simpledfm,y=c(-1,NA,NA,1),model="wordscores")
+
+predict(simplews)
+
