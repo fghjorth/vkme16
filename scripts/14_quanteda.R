89ff1e568f1c79746be6ad29e47748b146fe5a53
Frederik Hjorth <frederikhjorth@gmail.com>
2016-12-10 18:11:53 +0100

    shuffle around

--- scripts/14_quanteda.R
+++ scripts/14_quanteda.R
@@ -98,3 +98,22 @@ ggplot(royal_ws_df,aes(year,textscore_raw)) +
   theme_bw()
 
 
+####
+# REGNEEKSEMPEL FRA SLIDES
+####
+
+simpletexts<-c("velfærd velfærd velfærd",
+               "velfærd velfærd vækst",
+               "velfærd vækst vækst",
+               "vækst vækst vækst")
+simplecorpus<-corpus(simpletexts,docnames=c("el","s","v","la"))
+simpledfm<-dfm(simpletexts)
+
+#tf-idf
+tfidf(simpledfm)
+
+#regner efter i hånden
+tf<-3
+idf<- log10( 4/3 )
+tf*idf
+
--- scripts/14_textasdata.R
+++ scripts/14_textasdata.R
@@ -2,27 +2,5 @@ setwd("~/GitHub/vkme16/scripts")
 
 require(quanteda)
 
-#motiverende eksempel: meget simple tekster
 
-simpletexts<-c("velfærd velfærd velfærd",
-               "velfærd velfærd vækst",
-               "velfærd vækst vækst",
-               "vækst vækst vækst")
-simplecorpus<-corpus(simpletexts,docnames=c("el","s","v","la"))
-simpledfm<-dfm(simpletexts)
-
-#tf-idf
-
-tfidf(simpledfm)
-
-#regner efter i hånden
-tf<-3
-idf<- log10( 4/3 )
-tf*idf
-
-#wordscores eksempel
-
-simplews<-textmodel(x=simpledfm,y=c(-1,NA,NA,1),model="wordscores")
-
-predict(simplews)
 
